ff5aaf6aa1efd59466b44495efa047a21e28e74c
Tom van Dijk <t.vandijk@utwente.nl>
2012-12-17 10:57:41 +0100

    Added tagged LLCI methods; several 64-bit related fixes; parallel clear implementation fix

--- src/llci.h
+++ src/llci.h
@@ -32,10 +32,9 @@
 // 64-bit hashing function, http://www.locklessinc.com (hash_mul.s)
 unsigned long long hash_mul(const void* data, unsigned long long len);
 
-typedef struct llci
-{
+typedef struct llci {
     size_t             cache_size;  
-    uint32_t           mask;         // size-1
+    size_t             mask;         // size-1
     uint32_t           *table;       // table with hashes
     uint8_t            *data;        // table with data
     size_t             f_size;
@@ -50,29 +49,81 @@ typedef struct llci
 #define LLCI_CL_MASK       ((uint32_t)(~(LLCI_HASH_PER_CL-1)))
 #define LLCI_CL_MASK_R     ((uint32_t)(LLCI_HASH_PER_CL-1))
 
+static int __attribute__((unused)) llci_get_tag(const llci_t dbs, void *data) 
+{
+    size_t hash = (size_t)hash_mul(data, LLCI_KEYSIZE);
+    const size_t idx = hash & dbs->mask;
+    const size_t data_idx = idx * LLCI_PDS;
+
+    volatile uint32_t * const bucket = &dbs->table[idx];
+    const uint32_t v = *bucket;
+
+    hash >>= 32;
+
+    // abort if locked or has a different hash or has a tag of 0
+    if (v & LLCI_LOCK || ((v ^ hash) & 0x7FFFF000) || (v & 0x00000FFF) == 0) return 0;
+    // abort if key different
+    if (memcmp(&dbs->data[data_idx], data, LLCI_KEYSIZE) != 0) return 0;
+
+    memcpy(&((uint8_t*)data)[LLCI_KEYSIZE], &dbs->data[data_idx+LLCI_KEYSIZE], LLCI_DATASIZE-LLCI_KEYSIZE);
+    return (*bucket == v) ? 1 : 0;
+}
+
+static int __attribute__((unused)) llci_put_tag(const llci_t dbs, void *data) 
+{
+    size_t hash = (size_t)hash_mul(data, LLCI_KEYSIZE);
+    const size_t idx = hash & dbs->mask;
+    const size_t data_idx = idx * LLCI_PDS;
+
+    volatile uint32_t * const bucket = &dbs->table[idx];
+    const uint32_t v = *bucket;
+
+    hash >>= 32;
+
+    // abort if locked or exists
+    if (v & LLCI_LOCK) return 0;
+    if (!((v ^ hash) & 0x7FFFF000)) {
+        if (memcmp(&dbs->data[data_idx], data, LLCI_KEYSIZE) == 0) {
+            // Probably exists. (No lock)
+            return 0;
+        }
+    }
+
+    uint32_t hh = (v+1) & 0x00000FFF;
+    if (!hh) hh++; // Skip tag 0
+    hh |= (hash & 0x7FFFF000);
+
+    if (!cas(bucket, v, hh|LLCI_LOCK)) return 0;
+
+    memcpy(&dbs->data[data_idx], data, LLCI_DATASIZE);
+    *bucket = hh;
+    return 1; 
+}
+
 static int __attribute__((unused)) llci_get(const llci_t dbs, void *data) 
 {
-    uint32_t hash = (uint32_t)(hash_mul(data, LLCI_KEYSIZE) & LLCI_MASK);
-    if (hash == 0) hash++; // Do not use bucket 0.
+    size_t hash = hash_mul(data, LLCI_KEYSIZE);
+    const size_t idx = hash & dbs->mask;
+    const size_t data_idx = idx * LLCI_PDS;
 
-    uint32_t idx = hash & dbs->mask;
-
-    volatile uint32_t *bucket = &dbs->table[idx];
+    volatile uint32_t * const bucket = &dbs->table[idx];
     register uint32_t v = *bucket & LLCI_MASK;
 
+    hash >>= 32;
+    hash &= LLCI_MASK;
+    if (hash == 0) hash++;
+
+    // if locked or if different hash...
     if (v != hash) return 0;
 
+    // acquire lock
     if (!cas(bucket, v, v|LLCI_LOCK)) return 0;
 
-    // Lock acquired, compare
-    const size_t data_idx = idx * LLCI_PDS;
     if (memcmp(&dbs->data[data_idx], data, LLCI_KEYSIZE) == 0) {
-        // Found existing
         memcpy(&((uint8_t*)data)[LLCI_KEYSIZE], &dbs->data[data_idx+LLCI_KEYSIZE], LLCI_DATASIZE-LLCI_KEYSIZE);
         *bucket = v;
         return 1;                    
     } else {
-        // Did not match, release bucket again
         *bucket = v;
         return 0;
     }
@@ -80,51 +131,51 @@ static int __attribute__((unused)) llci_get(const llci_t dbs, void *data)
 
 static int __attribute__((unused)) llci_get_restart(const llci_t dbs, void *data) 
 {
-    uint32_t hash = (uint32_t)(hash_mul(data, LLCI_KEYSIZE) & LLCI_MASK);
-    if (hash == 0) hash++; // Do not use bucket 0.
+    size_t hash = hash_mul(data, LLCI_KEYSIZE);
+    const size_t idx = hash & dbs->mask;
+    const size_t data_idx = idx * LLCI_PDS;
 
-    uint32_t idx = hash & dbs->mask;
+    volatile uint32_t * const bucket = &dbs->table[idx];
 
-    volatile uint32_t *bucket = &dbs->table[idx];
+    hash >>= 32;
+    hash &= LLCI_MASK;
+    if (hash == 0) hash++;
 
     while (1) {
-        register uint32_t v = *bucket;
-        register uint32_t vh = v & LLCI_MASK;
-        if (vh != hash) return 0;
-        if (v == vh) { // v&LOCK == 0
-            if (!cas(bucket, vh, vh|LLCI_LOCK)) continue; // Atomic restart
+        const uint32_t v = *bucket;
+        // if different hash, abort
+        if ((v & LLCI_MASK) != hash) return 0;
+        if (v & LLCI_LOCK) continue;
+
+        // acquire lock
+        if (!cas(bucket, v, v|LLCI_LOCK)) continue; // atomic restart
             
-            // Lock acquired, compare
-            const size_t data_idx = idx * LLCI_PDS;
-            const uint8_t *bdata = &dbs->data[data_idx];
-            if (memcmp(bdata, data, LLCI_KEYSIZE) == 0) {
-                // Found existing
-                memcpy(&((uint8_t*)data)[LLCI_KEYSIZE], &bdata[LLCI_KEYSIZE], LLCI_DATASIZE-LLCI_KEYSIZE);
-                *bucket = vh;
-                return 1;                    
-            } else {
-                // Did not match, release bucket again
-                *bucket = vh;
-                return 0;
-            }
+        if (memcmp(&dbs->data[data_idx], data, LLCI_KEYSIZE) == 0) {
+            memcpy(&((uint8_t*)data)[LLCI_KEYSIZE], &dbs->data[data_idx+LLCI_KEYSIZE], LLCI_DATASIZE-LLCI_KEYSIZE);
+            *bucket = v;
+            return 1;                    
+        } else {
+            *bucket = v;
+            return 0;
         }
     }
 }
 
 static int __attribute__((unused)) llci_get_seq(const llci_t dbs, void *data) 
 {
-    uint32_t hash = (uint32_t)(hash_mul(data, LLCI_KEYSIZE) & LLCI_MASK);
-    if (hash == 0) hash++; // Do not use bucket 0.
+    size_t hash = hash_mul(data, LLCI_KEYSIZE);
+    const size_t idx = hash & dbs->mask;
+    const size_t data_idx = idx * LLCI_PDS;
 
-    uint32_t idx = hash & dbs->mask;
+    volatile uint32_t * const bucket = &dbs->table[idx];
+    const uint32_t v = *bucket & LLCI_MASK;
 
-    volatile uint32_t *bucket = &dbs->table[idx];
-    register uint32_t v = *bucket & LLCI_MASK;
+    hash >>= 32;
+    hash &= LLCI_MASK;
+    if (hash == 0) hash++;
 
     if (v != hash) return 0;
 
-    // Lock acquired, compare
-    const size_t data_idx = idx * LLCI_PDS;
     if (memcmp(&dbs->data[data_idx], data, LLCI_KEYSIZE) != 0) return 0;
 
     // Found existing
@@ -134,27 +185,18 @@ static int __attribute__((unused)) llci_get_seq(const llci_t dbs, void *data)
 
 static int __attribute__((unused)) llci_put(const llci_t dbs, void *data) 
 {
-    uint32_t hash = (uint32_t)(hash_mul(data, LLCI_KEYSIZE) & LLCI_MASK);
-    if (hash == 0) hash++; // Avoid 0.
+    size_t hash = hash_mul(data, LLCI_KEYSIZE);
+    const size_t idx = hash & dbs->mask;
+    const size_t data_idx = idx * LLCI_PDS;
     
-    register uint32_t idx = hash & dbs->mask; // fast version of hash & tableSize
-    register size_t data_idx = idx * LLCI_PDS;
-
-    register volatile uint32_t *bucket = &dbs->table[idx];
+    volatile uint32_t * const bucket = &dbs->table[idx];
     register uint32_t v = *bucket;
 
-    if (v & LLCI_LOCK) return 0; // Not added
-    v &= LLCI_MASK;
+    hash >>= 32;
+    hash &= LLCI_MASK;
+    if (hash == 0) hash++;
 
-    if (v == LLCI_EMPTY) {
-        if (cas(bucket, LLCI_EMPTY, hash|LLCI_LOCK)) {
-            memcpy(&dbs->data[data_idx], data, LLCI_DATASIZE);
-            *bucket = hash;
-            return 1; // Added
-        } else {
-            return 0;
-        }
-    }
+    if (v & LLCI_LOCK) return 0; // Not added
 
     if (v == hash) {
         if (memcmp(&dbs->data[data_idx], data, LLCI_KEYSIZE) == 0) {
@@ -163,26 +205,24 @@ static int __attribute__((unused)) llci_put(const llci_t dbs, void *data)
         }
     }
 
-    if (cas(bucket, v, hash|LLCI_LOCK)) {
-        memcpy(&dbs->data[data_idx], data, LLCI_DATASIZE);
-        *bucket = hash;
-        return 1; // Added
-    } else {
-        // Claim failed, never mind
-        return 0;
-    }
+    if (!cas(bucket, v, hash|LLCI_LOCK)) return 0;
+    memcpy(&dbs->data[data_idx], data, LLCI_DATASIZE);
+    *bucket = hash;
+    return 1; // Added
 }
 
 static int __attribute__((unused)) llci_put_seq(const llci_t dbs, void *data) 
 {
-    uint32_t hash = (uint32_t)(hash_mul(data, LLCI_KEYSIZE) & LLCI_MASK);
-    if (hash == 0) hash++; // Avoid 0.
+    size_t hash = hash_mul(data, LLCI_KEYSIZE);
+    const size_t idx = hash & dbs->mask;
+    const size_t data_idx = idx * LLCI_PDS;
     
-    register uint32_t idx = hash & dbs->mask; // fast version of hash & tableSize
-    register size_t data_idx = idx * LLCI_PDS;
-
-    register volatile uint32_t *bucket = &dbs->table[idx];
+    volatile uint32_t * const bucket = &dbs->table[idx];
     register uint32_t v = *bucket;
+
+    hash >>= 32;
+    hash &= LLCI_MASK;
+    if (hash == 0) hash++;
 
     if (v == hash) {
         if (memcmp(&dbs->data[data_idx], data, LLCI_KEYSIZE) == 0) {
@@ -211,21 +251,27 @@ static inline llci_t llci_create(size_t cache_size)
     if (cache_size < LLCI_HASH_PER_CL) cache_size = LLCI_HASH_PER_CL;
 
     // Cache size must be a power of 2
-    assert(llci_next_pow2(cache_size) == cache_size);
-    dbs->cache_size = cache_size;
+    if (llci_next_pow2(cache_size) != cache_size) {
+        fprintf(stderr, "LLCI: Table size must be a power of 2!\n");
+        exit(1);
+    }
 
+    dbs->cache_size = cache_size;
     dbs->mask = dbs->cache_size - 1;
 
     dbs->table = mmap(0, dbs->cache_size * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, 0, 0);
     dbs->data = mmap(0, dbs->cache_size * LLCI_PDS, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, 0, 0);
-    if (dbs->table == (uint32_t*)-1 || dbs->data == (uint8_t*)-11) { 
-      fprintf(stderr, "Unable to allocate memory!"); 
-      exit(1);
+    if (dbs->table == (uint32_t*)-1 || dbs->data == (uint8_t*)-1) { 
+        fprintf(stderr, "LLCI: Unable to allocate memory!\n"); 
+        exit(1);
     }
 
 #ifdef HAVE_NUMA_H
     size_t f_size=0;
     numa_interleave(dbs->table, dbs->cache_size * sizeof(uint32_t), &f_size);
+    if (f_size % (sizeof(uint32_t)*(LINE_SIZE))) {
+        fprintf(stderr, "LLCI: f_size not properly aligned to processor cache lines!");
+    }
     dbs->f_size = (f_size /= sizeof(uint32_t));
     f_size *= LLCI_PDS;
     numa_interleave(dbs->data, dbs->cache_size * LLCI_PDS, &f_size);
@@ -238,8 +284,8 @@ static inline void __attribute__((unused))
 llci_clear_partial(const llci_t dbs, size_t first, size_t count) 
 {
     if (first >= 0 && first < dbs->cache_size) {
-        if (count + first > dbs->cache_size) count = dbs->cache_size - first;
-        memset(&dbs->table[first], 0, 4 * count);
+        if (count > dbs->cache_size - first) count = dbs->cache_size - first;
+        memset(dbs->table + first, 0, 4 * count);
     }
 }
 
@@ -253,13 +299,16 @@ llci_clear_multi(const llci_t dbs, size_t my_id, size_t n_workers)
     int node, node_index, index, total;
     numa_worker_info(my_id, &node, &node_index, &index, &total);
     // we only clear that of our own node...
-    size_t cachelines_total = (dbs->f_size      + LINE_SIZE - 1) / (LINE_SIZE);
-    size_t cachelines_each  = (cachelines_total + total     - 1) / total;
-    size_t first            = node_index * dbs->f_size + index * cachelines_each * LINE_SIZE;
-    size_t max              = cachelines_total - index * cachelines_each;
-    if (max > 0) {
-        size_t count = max > cachelines_each ? cachelines_each : max;
-        llci_clear_partial(dbs, first / 4, count / 4);
+    // note f_size is the number of buckets, and is aligned to page_size...
+    // with page_size 4096 bytes, f_size is aligned on 1024 buckets = 16 cachelines (typically)
+    size_t cachelines_total = dbs->f_size / LLCI_HASH_PER_CL;
+    size_t cachelines_each  = (cachelines_total + total - 1) / total;
+    size_t first_line       = node_index * dbs->f_size / LLCI_HASH_PER_CL + index * cachelines_each;
+    size_t max_lines        = cachelines_total - index * cachelines_each;
+    if (max_lines > 0) {
+        size_t count = max_lines > cachelines_each ? cachelines_each : max_lines;
+        // Note that llci_clear_partial will fix count if overflow...
+        llci_clear_partial(dbs, first_line * LLCI_HASH_PER_CL, count * LLCI_HASH_PER_CL);
     }
 #else
     size_t cachelines_total = (dbs->cache_size  + LLCI_HASH_PER_CL - 1) / LLCI_HASH_PER_CL;
@@ -284,9 +333,8 @@ static inline void llci_free(const llci_t dbs)
 
 static inline void __attribute__((unused)) llci_print_size(const llci_t dbs, FILE *f) 
 {
-    fprintf(f, "Hash: %ld * 4 = %ld bytes; Data: %ld * %ld = %ld bytes",
-        dbs->cache_size, dbs->cache_size * 4, dbs->cache_size, 
-        LLCI_PDS, dbs->cache_size * LLCI_PDS);
+    fprintf(f, "LLCI table with %ld entries; hash array = %ld bytes; data array = %ld bytes",
+        dbs->cache_size, dbs->cache_size * 4, dbs->cache_size * LLCI_PDS);
 }
 
 #endif

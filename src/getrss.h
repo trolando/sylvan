a4218452fd7c80ee5ddbc81b489f3c2e2a30286c
Tom van Dijk <t.vandijk@utwente.nl>
2015-02-03 15:49:53 +0100

    Add getrss.h and getrss.c to makefiles

---
+++ src/getrss.h
@@ -0,0 +1,17 @@
+#ifndef GETRSS_H
+#define GETRSS_H
+
+/**
+ * Returns the peak (maximum so far) resident set size (physical
+ * memory use) measured in bytes, or zero if the value cannot be
+ * determined on this OS.
+ */
+size_t getPeakRSS();
+
+/**
+ * Returns the current resident set size (physical memory use) measured
+ * in bytes, or zero if the value cannot be determined on this OS.
+ */
+size_t getCurrentRSS();
+
+#endif

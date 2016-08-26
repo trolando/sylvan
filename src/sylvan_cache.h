28fe86672296645e874ae14ed9b2f294d518f458
Tom van Dijk <tom@tvandijk.nl>
2016-08-26 11:55:41 +0200

    Updated copyright information

--- src/sylvan_cache.h
+++ src/sylvan_cache.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2011-2016 Formal Methods and Tools, University of Twente
+ * Copyright 2016 Tom van Dijk, Johannes Kepler University Linz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <stdint.h> // for uint32_t etc
 
 #include <sylvan_config.h>

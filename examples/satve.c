775ed94f896037099b0046e2b1d3036664cc92bb
Tom van Dijk <t.vandijk@gmail.com>
2017-07-03 13:21:34 +0200

    improvements

--- examples/satve.c
+++ examples/satve.c
@@ -9,6 +9,13 @@
 #include <getrss.h>
 #include <sylvan_int.h>
 
+/**
+ * TODO
+ * - implement trace output and input
+ * - variable selection heuristics: smallest #nodes, smallest #clauses, largest #clauses
+ * - convert ZDD to CNF on the graph
+ */
+
 /* Configuration */
 static int workers = 0; // autodetect
 static int verbose = 0;
@@ -21,26 +28,34 @@ static int bound = 0;
 static int death_threshold = 100;
 static int nodebound = 0;
 static int clausebound = 0;
+static int inflate = 0;
 static char* cnf_filename = NULL; // filename of CNF
 static char* out_filename = NULL; // filename of output
 static char* dot_filename = NULL; // filename of DOT file
-
-// TODO load/save variable elimination order
+static char* trace_filename = NULL; // filename of trace file
+static char* tracein_filename = NULL; // filename of trace file
 
 /* argp configuration */
 static struct argp_option options[] =
 {
+    {0, 0, 0, 0, "Basic program options", 1},
     {"workers", 'w', "<workers>", 0, "Number of workers (default=0: autodetect)", 0},
-    {"output", 'o', "<output file>", 0, "Write result to CNF", 0},
-    {"verbose", 'v', 0, 0, "Set verbose", 0},
-    {"dot", 'd', "<dot file>", 0, "Write CNF to DOT", 0},
-    {"parsetobdd", 1, 0, 0, "Construct BDD during parsing", 0},
-    {"tobdd", 2, 0, 0, "After parsing CNF, convert to BDD", 0},
-    {"isoc", 3, 0, 0, "Use BDD into ISOC to compute variable elimination", 0},
-    {"qmc", 4, "<factor>", 0, "Use QMC resolution whenever the ZDD has grown by <factor>", 0},
-    {"nodebound", 5, 0, 0, "Bounded VE based on #nodes", 0},
-    {"clausebound", 6, 0, 0, "Bounded VE based on #clauses", 0},
-    {"memory", 'm', "<megabytes>", 0, "How many MB memory for nodes+operations", 0},
+    {"memory", 'm', "<megabytes>", 0, "How many MB memory for nodes table plus operation cache", 0},
+    {"verbose", 'v', 0, 0, "Set output to verbose", 0},
+    {0, 0, 0, 0, "Parsing and preprocessing", 2},
+    {"parsetobdd", 1, 0, 0, "During parsing, construct BDD to compute SAT/UNSAT", 2},
+    {"tobdd", 2, 0, 0, "After parsing, construct BDD to compute SAT/UNSAT", 2},
+    {"dot", 'd', "<dot file>", 0, "After parsing, write DOT file of the clause ZDD", 2},
+    {"inflate", 7, "<cycles>", 0, "After parsing, inflate the clause set for <cycles> cycles", 2},
+    {0, 0, 0, 0, "Variable elimination", 3},
+    {"isoc", 3, 0, 0, "Compute VE using the ISOC algorithm on the BDD of the variable environment", 3},
+    {"qmc", 4, "<factor>", 0, "Use QMC-style resolution whenever the ZDD has grown by <factor>", 3},
+    {"nodebound", 5, 0, 0, "Perform bounded variable elimination based on #nodes", 3},
+    {"clausebound", 6, 0, 0, "Perform bounded variable elimination based on #clauses", 3},
+    {0, 0, 0, 0, "Other options", 4},
+    {"trace", 't', "<trace file>", 0, "Write action trace to file", 4},
+    {"tracein", 'i', "<trace file>", 0, "Read action trace from file", 4},
+    {"output", 'o', "<output file>", 0, "Write result to CNF", 4},
     {0, 0, 0, 0, 0, 0}
 };
 
@@ -63,6 +78,12 @@ parse_opt(int key, char *arg, struct argp_state *state)
     case 'm':
         memory = atoi(arg);
         break;
+    case 't':
+        trace_filename = arg;
+        break;
+    case 'i':
+        tracein_filename =arg;
+        break;
     case 1:
         parsetobdd = 1;
         break;
@@ -80,6 +101,9 @@ parse_opt(int key, char *arg, struct argp_state *state)
         break;
     case 6:
         clausebound = 1;
+        break;
+    case 7:
+        inflate = atoi(arg);
         break;
     case ARGP_KEY_ARG:
         if (state->arg_num == 0) cnf_filename = arg;
@@ -241,9 +265,143 @@ TASK_1(ZDD, parse_cnf_file, FILE*, file)
         last_literal = lit;
     }
 
+    INFO("Read %s, %d variables, %d clauses, %d literals.\n", cnf_filename, nvars, nclauses, nlits);
+
     // printf("c total number of literals: %d\n", nlits);
     free(lits);
     zdd_refs_popptr(1);
+    return db;
+}
+
+/**
+ * Helper function for parallel recursive CNF-to-ZDD conversion
+ */
+TASK_4(ZDD, arr_to_zdd, int*, lits, int*, indices, int, first, int, count)
+{
+    if (count <= 32) {
+        ZDD db = zdd_false;
+        zdd_refs_pushptr(&db);
+        for (int i=0; i<count; i++) db = zdd_add_clause(db, lits+indices[first+i]);
+        zdd_refs_popptr(1);
+        return db;
+    } else {
+        zdd_refs_spawn(SPAWN(arr_to_zdd, lits, indices, first, count/2));
+        ZDD db1 = zdd_refs_push(CALL(arr_to_zdd, lits, indices, first + count/2, count - count/2));
+        ZDD db2 = zdd_refs_push(zdd_refs_sync(SYNC(arr_to_zdd)));
+        db1 = zdd_or(db1, db2);
+        zdd_refs_pop(2);
+        return db1;
+    }
+}
+
+/**
+ * Parser.
+ * First read CNF into int array.
+ * Then convert int array to ZDD.
+ */
+TASK_1(ZDD, parse_cnf_file_2, FILE*, file)
+{
+    /* parse header */
+    while (1) {
+        int ch = fgetc(file);
+        if (ch == EOF) {
+            Abort("unexpected end-of-file");
+        } else if (ch == 'c') {
+            while ((ch = fgetc(file)) != '\n') {
+                if (ch == EOF) Abort("unexpected end-of-file");
+            }
+            continue;
+        } else if (ch != 'p') {
+            Abort("unexpected characters");
+        }
+        if ((fscanf (file, " cnf %d %d", &nvars, &nclauses)) != 2) {
+            Abort("invalid header");
+        }
+        if (nvars < 0 || nclauses < 0) {
+            Abort("invalid header");
+        }
+        break;
+    }
+    
+    /* setup arrays */
+    int lits_size = 64, lits_count = 0;
+    int *lits = (int*)malloc(sizeof(int[lits_size]));
+    if (lits == NULL) Abort("out of memory");
+    int *indices = (int*)malloc(sizeof(int[nclauses]));
+
+    /* parse clauses */
+    int read_clauses = 0;
+    int last_literal = 0;
+    int lits_first = 0;
+    nlits = 0;
+
+    while (1) {
+        int ch = fgetc(file);
+        /* skip whitespace */
+        if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r') continue;
+        /* skip comment lines */
+        if (ch == 'c') {
+            while ((ch = fgetc(file)) != '\n') {
+                if (ch == EOF) Abort("unexpected end-of-file");
+            }
+            continue;
+        }
+        /* if EOF, check if good */
+        if (ch == EOF) {
+            if (last_literal != 0) Abort("zero missing");
+            if (read_clauses < nclauses) Abort("clause(s) missing");
+            break;
+        }
+        /* read next sign or digit */
+        int sign;
+        if (ch == '-') {
+            sign = -1;
+            ch = fgetc(file);
+            if (!isdigit(ch)) Abort("expected digit after '-'");
+        } else {
+            sign = 1;
+            if (!isdigit (ch)) Abort("expected digit or '-'");
+        }
+        int lit = ch - '0';
+        /* parse rest of literal */
+        while (isdigit(ch = fgetc(file))) lit = 10*lit + (ch - '0');
+        if (lit > nvars) Abort("variable exceeds maximum");
+        if (read_clauses >= nclauses) Abort("number of clauses more than expected");
+        /* increase array size if needed */
+        if (lits_count == lits_size) {
+            lits_size += lits_size/2; // grow by factor 150%
+            lits = (int*)realloc(lits, sizeof(int[lits_size]));
+            if (lits == NULL) Abort("out of memory");
+        }
+        if (lit != 0) {
+            /* add literal to array */
+            lits[lits_count++] = lit * sign;
+            nlits++;
+        } else {
+            /* sort the literals by variable */
+            qsort(lits+lits_first, lits_count-lits_first, sizeof(int), literal_compare);
+            /* check for tautology or same variables */
+            for (int i=lits_first; i<lits_count-1; i++) {
+                if (lits[i] == lits[i+1] || lits[i] == -lits[i+1]) Abort("variables twice in clause");
+            }
+            /* add clause to database */
+            lits[lits_count++] = 0;
+            indices[read_clauses++] = lits_first;
+            lits_first = lits_count;
+        }
+        /* set last literal and continue loop */
+        last_literal = lit;
+    }
+
+    //if (verbose) INFO("Read literals read %d clauses read %d expected %d\n", nlits, read_clauses, nclauses);
+    INFO("Read %s, %d variables, %d clauses, %d literals.\n", cnf_filename, nvars, nclauses, nlits);
+
+
+    /* start with empty clause database */
+    
+    ZDD db = CALL(arr_to_zdd, lits, indices, 0, read_clauses);
+
+    free(lits);
     return db;
 }
 
@@ -486,7 +644,7 @@ TASK_0(ZDD, read_input_cnf)
             }
         }
 
-        db = CALL(parse_cnf_file, f);
+        db = CALL(parse_cnf_file_2, f);
 
         // Close file
         fclose(f);
@@ -504,10 +662,8 @@ TASK_0(ZDD, read_input_cnf)
                 exit(0);
             }
         }
-        db = CALL(parse_cnf_file, stdin);
+        db = CALL(parse_cnf_file_2, stdin);
     }
-
-    INFO("Read %s, %d variables, %d clauses, %d literals.\n", cnf_filename, nvars, nclauses, nlits);
 
     return db;
 }
@@ -543,10 +699,10 @@ main(int argc, char **argv)
     zdd_refs_pushptr(&db);
 
     // Report the number of clauses and nodes
-    {
+    if (1) {
         double n_clauses = zdd_satcount(db);
         size_t n_nodes = zdd_nodecount(&db, 1);
-        INFO("After loading CNF: %.0lf clauses using %zu nodes.\n", n_clauses, n_nodes);
+        INFO("After converting CNF to ZDD: %.0lf clauses using %zu nodes.\n", n_clauses, n_nodes);
     }
 
     if (tobdd) {
@@ -565,10 +721,10 @@ main(int argc, char **argv)
     db = zdd_clause_self_subsume(db);
 
     // Report the number of clauses and nodes
-    {
+    if (1) {
         double n_clauses = zdd_satcount(db);
         size_t n_nodes = zdd_nodecount(&db, 1);
-        INFO("After self-subsumption: %.0lf clauses using %zu nodes.\n", n_clauses, n_nodes);
+        INFO("After performing self-subsumption: %.0lf clauses using %zu nodes.\n", n_clauses, n_nodes);
     }
 
     if (dot_filename != NULL) {
@@ -613,7 +769,7 @@ main(int argc, char **argv)
     }
 
     // Report the number of clauses and nodes
-    {
+    if (1) {
         double n_clauses = zdd_satcount(db);
         size_t n_nodes = zdd_nodecount(&db, 1);
         INFO("After initial unit propagation: %.0lf clauses using %zu nodes.\n", n_clauses, n_nodes);
@@ -631,13 +787,15 @@ main(int argc, char **argv)
     const double orig_db_clauses = db_clauses;
     const size_t orig_db_nodes = db_nodes;
 
-
-
     /**
      * This is of course a totally strange thing
      */
 
-    for (;;) {
+    if (inflate > 0) {
+        INFO("Inflating %d times!\n", inflate);
+    }
+
+    for (int ii=0; ii<inflate; ii++) {
         ZDD bucket = zdd_false; // empty
         zdd_refs_pushptr(&bucket);
 
@@ -691,9 +849,11 @@ main(int argc, char **argv)
             /**
              * Report
              */
-            //double bucket_clauses = zdd_satcount(bucket);
-            //size_t bucket_nodes = zdd_nodecount(&bucket, 1);
-            //INFO("\033[1;36mUpdated\033[m var %d/%d %.0f clauses (%zd nodes)\n", i, nvars, bucket_clauses, bucket_nodes);
+            if (verbose) {
+                double bucket_clauses = zdd_satcount(bucket);
+                size_t bucket_nodes = zdd_nodecount(&bucket, 1);
+                INFO("\033[1;36mUpdated\033[m var %d/%d %.0f clauses (%zd nodes)\n", i, nvars, bucket_clauses, bucket_nodes);
+            }
         }
 
         /**
@@ -715,17 +875,125 @@ main(int argc, char **argv)
         INFO("After QMC-style resolution: %.0f clauses (%zd nodes)\n", zdd_satcount(db), zdd_nodecount(&db, 1));
     }
 
-
-
-
-
-
-
-
     size_t last_qmc_size = db_nodes;
 
     int elim_total = 0;
     int i = 1;
+
+    if (tracein_filename != 0) {
+        FILE *fin = fopen(tracein_filename, "r");
+        if (fin == NULL) Abort("cannot read tracein file");
+        while (!feof(fin)) {
+            int i = 0;
+            char ch;
+            while (!feof(fin)) {
+                ch = fgetc(fin);
+                if (ch != ' ' && ch != '\n') break;
+            }
+            if (feof(fin)) break;
+            if (ch == 'q') {
+                db = zdd_clause_qmc(db);
+                db_clauses = zdd_satcount(db);
+                db_nodes = zdd_nodecount(&db, 1);
+
+                INFO("After \033[1;32mQMC-style resolution\033[m %g clauses (%zd nodes)\n", db_clauses, db_nodes);
+                continue;
+            }
+            if (ch != 'e') Abort("tracefile error\n");
+
+            if (fscanf(fin, "%d", &i) != 1) break;
+
+            /**
+             * Compute the environment of the current variable
+             */
+            ZDD lits = zdd_set_from_array((uint32_t[]){2*i, 2*i+1}, 2);
+            zdd_refs_push(lits);
+            ZDD env = zdd_clause_environment(db, lits);
+            zdd_refs_push(env);
+
+            if (env == zdd_false) continue; // nothing to do
+
+            if (useisoc) {
+                /**
+                 * Compute the satisfying assignments for the current environment
+                 */
+                MTBDD sat = zdd_clause_sat(env, mtbdd_true);
+                mtbdd_refs_push(sat);
+
+                /**
+                 * Eliminate variable
+                 */
+                MTBDD quantified = sylvan_exists(sat, sylvan_ithvar(i));
+                mtbdd_refs_push(quantified);
+
+                /**
+                 * Run ISOC algorithm
+                 */
+                MTBDD q_check;
+                reduced = zdd_clause_isoc(quantified, quantified, &q_check);
+
+                mtbdd_refs_pop(2);
+            } else {
+                ZDD cof_n = zdd_clause_cof(env, 2*i);
+                zdd_refs_push(cof_n);
+                ZDD cof_p = zdd_clause_cof(env, 2*i+1);
+                zdd_refs_push(cof_p);
+                reduced = zdd_clause_distribution(cof_n, cof_p);
+                zdd_refs_pop(2);
+            }
+
+            /**
+             * Replace clauses
+             */
+            old_db = db;
+            db = zdd_diff(db, env);
+            db = zdd_clause_union(db, reduced);
+
+            zdd_refs_pop(2); // lits, env
+
+            // Find all unit clauses in the clause database
+            units = zdd_clause_units(db);
+            if (units == zdd_false) {
+                Abort("The empty clause has been found!\n");
+            }
+
+            /**
+             * Propagate units
+             */
+            if (units != zdd_true) {
+                // Repeat until no more new units are found
+                while (units != zdd_true) {
+                    if (verbose) INFO("Found %zu new units!\n", zdd_set_count(units));
+                    // Add the new units to all_units and check for contradictions
+                    all_units = zdd_set_union(all_units, units);
+                    if (zdd_clause_units_contradict(all_units)) {
+                        Abort("Units contradict! Aborting. (UNSAT)\n");
+                    }
+                    // Then perform unit propagation of the new units
+                    db = zdd_clause_up(db, units);
+                    // See if there are new units now
+                    units = zdd_clause_units(db);
+                    if (units == zdd_false) {
+                        Abort("The empty clause has been found! Aborting. (UNSAT)\n");
+                    }
+                }
+            }
+
+            db_clauses = zdd_satcount(db);
+            db_nodes = zdd_nodecount(&db, 1);
+
+            //INFO("After \033[1;32mvariable elimination\033[m of %d %g clauses (%zd nodes)\n", i, db_clauses, db_nodes);
+            INFO("After \033[1;32mvariable elimination\033[m %g clauses (%zd nodes)\n", db_clauses, db_nodes);
+        }
+
+        if (db == zdd_true) {
+            Abort("Empty clause! (UNSAT)\n");
+        } else if (db == zdd_false) {
+            Abort("No clause! (SAT)\n");
+        }
+    }
+
+    FILE* tracefile = (trace_filename != 0) ? fopen(trace_filename, "w") : NULL;
 
     for (;;) {
         int minbound = -1;
@@ -831,20 +1099,25 @@ main(int argc, char **argv)
              * Make Quine Free
              */
 
-            (void)last_qmc_size;
+            double new_db_clauses = 0;
+            size_t new_db_nodes = 0;
+            int did_qmc = 0;
+
             if (qmc != -1 && (last_qmc_size*qmc < db_nodes)) {
                 ZDD older_db = db;
                 db = zdd_clause_qmc(db);
-                double new_db_clauses = zdd_satcount(db);
-                size_t new_db_nodes = zdd_nodecount(&db, 1);
+                new_db_clauses = zdd_satcount(db);
+                new_db_nodes = zdd_nodecount(&db, 1);
 
                 if (db_nodes < new_db_nodes ){
                     if (verbose) INFO("Skip QMC-style resolution (%.0f to %.0f clauses, %zd to %zd nodes).\n", db_clauses, new_db_clauses, db_nodes, new_db_nodes);
                     db = older_db;
                 } else if (db_clauses != new_db_clauses || db_nodes != new_db_nodes) {
                     if (verbose) INFO("After QMC-style resolution: from %.0f to %.0f clauses (%zd to %zd nodes)\n", db_clauses, new_db_clauses, db_nodes, new_db_nodes);
+                    did_qmc = 1;
                 } else {
                     if (verbose) INFO("Skip QMC-style resolution (no change).\n");
+                    db = older_db;
                 }
 
                 last_qmc_size = db_nodes;
@@ -881,41 +1154,17 @@ main(int argc, char **argv)
                 db_nodes = old_db_nodes;
                 continue;
             } else {
-                INFO("\033[1;36mEliminated\033[m var %d (%d/%d) from %g to %g clauses (%zd to %zd nodes)\n", i, elim_total+1, nvars, old_db_clauses, db_clauses, old_db_nodes, db_nodes);
+                if (tracefile != NULL) fprintf(tracefile, "e %d\n", i);
+                if (did_qmc) {
+                    if (tracefile != NULL) fprintf(tracefile, "q\n");
+                    INFO("\033[1;32mEliminated\033[m var %d (%d/%d) from %g to %g to %g clauses (%zd to %zd to %zd nodes)\n", i, elim_total+1, nvars, old_db_clauses, new_db_clauses, db_clauses, old_db_nodes, new_db_nodes, db_nodes);
+                } else {
+                    INFO("\033[1;32mEliminated\033[m var %d (%d/%d) from %g to %g clauses (%zd to %zd nodes)\n", i, elim_total+1, nvars, old_db_clauses, db_clauses, old_db_nodes, db_nodes);
+                }
                 old_db = zdd_false; // deref
                 elim++;
                 elim_total++;
             }
-
-            /**
-             * Make Quine Free
-             */
-
-            /*
-            (void)last_qmc_size;
-            if (1){//qmc && (last_qmc_size*qmc < db_nodes)) {
-                old_db = db;
-                db = zdd_clause_qmc(db);
-                old_db_clauses = db_clauses;
-                old_db_nodes = db_nodes;
-                db_clauses = zdd_satcount(db);
-                db_nodes = zdd_nodecount(&db, 1);
-
-                if (old_db_nodes < db_nodes ){
-                    INFO("After QMC-style resolution: skip from %f to %f clauses, %zd to %zd nodes.\n", old_db_clauses, db_clauses, old_db_nodes, db_nodes);
-                    db = old_db;
-                    db_clauses = old_db_clauses;
-                    db_nodes = old_db_nodes;
-                } else if (old_db_clauses != db_clauses || old_db_nodes != db_nodes) {
-                    INFO("After QMC-style resolution: from %f to %f clauses (%zd to %zd nodes)\n", old_db_clauses, db_clauses, old_db_nodes, db_nodes);
-                } else {
-                    INFO("After QMC-style resolution: no change\n");
-                }
-
-                old_db = zdd_false;
-
-                last_qmc_size = db_nodes;
-            }*/
 
             if (verbose) print_memory_usage();
 

d7f82b41ebead76c57d34606653aa58f9a6c8393
Tom van Dijk <tom@tvandijk.nl>
2022-06-19 11:50:41 +0200

    fix types of C++ object methods

--- src/sylvan_obj.hpp
+++ src/sylvan_obj.hpp
@@ -79,13 +79,13 @@ public:
      */
     static Bdd bddCube(const BddSet &variables, std::vector<uint8_t> values);
 
-    int operator==(const Bdd& other) const;
-    int operator!=(const Bdd& other) const;
+    bool operator==(const Bdd& other) const;
+    bool operator!=(const Bdd& other) const;
     Bdd& operator=(const Bdd& right);
-    int operator<=(const Bdd& other) const;
-    int operator>=(const Bdd& other) const;
-    int operator<(const Bdd& other) const;
-    int operator>(const Bdd& other) const;
+    bool operator<=(const Bdd& other) const;
+    bool operator>=(const Bdd& other) const;
+    bool operator<(const Bdd& other) const;
+    bool operator>(const Bdd& other) const;
     Bdd operator!() const;
     Bdd operator~() const;
     Bdd operator*(const Bdd& other) const;
@@ -104,22 +104,22 @@ public:
     /**
      * @brief Returns non-zero if this Bdd is bddOne() or bddZero()
      */
-    int isConstant() const;
+    bool isConstant() const;
 
     /**
      * @brief Returns non-zero if this Bdd is bddOne() or bddZero()
      */
-    int isTerminal() const;
+    bool isTerminal() const;
 
     /**
      * @brief Returns non-zero if this Bdd is bddOne()
      */
-    int isOne() const;
+    bool isOne() const;
 
     /**
      * @brief Returns non-zero if this Bdd is bddZero()
      */
-    int isZero() const;
+    bool isZero() const;
 
     /**
      * @brief Returns the top variable index of this Bdd (the variable in the root node)
@@ -189,7 +189,7 @@ public:
     /**
      * @brief Returns whether all elements in f are also in g
      */
-    int Leq(const Bdd& g) const;
+    bool Leq(const Bdd& g) const;
 
     /**
      * @brief Computes the reverse application of a transition relation to this set.
@@ -514,7 +514,7 @@ public:
     /**
      * @brief Returns non-zero when this map is empty
      */
-    int isEmpty() const;
+    bool isEmpty() const;
 };
 
 class MtbddMap;
@@ -592,8 +592,8 @@ public:
      */
     static Mtbdd mtbddCube(const BddSet &variables, std::vector<uint8_t> values, const Mtbdd &terminal);
 
-    int operator==(const Mtbdd& other) const;
-    int operator!=(const Mtbdd& other) const;
+    bool operator==(const Mtbdd& other) const;
+    bool operator!=(const Mtbdd& other) const;
     Mtbdd& operator=(const Mtbdd& right);
     Mtbdd operator!() const;
     Mtbdd operator~() const;
@@ -609,22 +609,22 @@ public:
     /**
      * @brief Returns non-zero if this Mtbdd is a leaf
      */
-    int isTerminal() const;
+    bool isTerminal() const;
 
     /**
      * @brief Returns non-zero if this Mtbdd is a leaf
      */
-    int isLeaf() const;
+    bool isLeaf() const;
 
     /**
      * @brief Returns non-zero if this Mtbdd is mtbddOne()
      */
-    int isOne() const;
+    bool isOne() const;
 
     /**
      * @brief Returns non-zero if this Mtbdd is mtbddZero()
      */
-    int isZero() const;
+    bool isZero() const;
 
     /**
      * @brief Returns the top variable index of this Mtbdd (the variable in the root node)
@@ -812,7 +812,7 @@ public:
     /**
      * @brief Returns non-zero when this map is empty
      */
-    int isEmpty();
+    bool isEmpty();
 };
 
 class Sylvan {

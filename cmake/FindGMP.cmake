c3dce1d2b3b4feee53bc44badce7f89a293fdd7f
Tom van Dijk <tom@tvandijk.nl>
2026-04-02 00:29:45 +0200

    Improvements to the build script

--- cmake/FindGMP.cmake
+++ cmake/FindGMP.cmake
@@ -1,24 +1,28 @@
-# Try to find GMP
-# Once done this will define:
-# - GMP_FOUND - True if the system has GMP
-# - GMP_INCLUDE_DIRS - include directories for compiling
-# - GMP_LIBRARIES - libraries for linking
-# - GMP_DEFINITIONS - cflags suggested by pkg-config
+# FindGMP.cmake
+# Provides an imported target GMP::GMP
 
-find_package(PkgConfig)
-pkg_check_modules(PC_GMP QUIET gmp)
-
-set(GMP_DEFINITIONS ${PC_GMP_CFLAGS_OTHER})
+find_package(PkgConfig QUIET)
+if(PKG_CONFIG_FOUND)
+    pkg_check_modules(PC_GMP QUIET gmp)
+endif()
 
 find_path(GMP_INCLUDE_DIR gmp.h
-          HINTS ${PC_GMP_INCLUDEDIR} ${PC_GMP_INCLUDE_DIRS})
+    HINTS ${PC_GMP_INCLUDEDIR} ${PC_GMP_INCLUDE_DIRS}
+)
 
-find_library(GMP_LIBRARIES NAMES gmp libgmp
-             HINTS ${PC_GMP_LIBDIR} ${PC_GMP_LIBRARY_DIRS})
+find_library(GMP_LIBRARY NAMES gmp libgmp
+    HINTS ${PC_GMP_LIBDIR} ${PC_GMP_LIBRARY_DIRS}
+)
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set GMP_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args(GMP DEFAULT_MSG GMP_LIBRARIES GMP_INCLUDE_DIR)
+find_package_handle_standard_args(GMP DEFAULT_MSG GMP_LIBRARY GMP_INCLUDE_DIR)
 
-mark_as_advanced(GMP_INCLUDE_DIR GMP_LIBRARIES)
+if(GMP_FOUND AND NOT TARGET GMP::GMP)
+    add_library(GMP::GMP UNKNOWN IMPORTED)
+    set_target_properties(GMP::GMP PROPERTIES
+        IMPORTED_LOCATION "${GMP_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${GMP_INCLUDE_DIR}"
+    )
+endif()
+
+mark_as_advanced(GMP_INCLUDE_DIR GMP_LIBRARY)
